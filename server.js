521df5e456d8aa1cfe93027bb28411512f64088b
azyMD <dgodorog@gmail.com>
2025-01-28 13:49:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ io.on('connection', (socket) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
+});
 });
